1454a20ab082f73a5aba4e489328dea6aa7dc470
julien.j <julien@vtest.com>
2024-01-05 10:27:56 +0100

    feat: add db & unit tests start

---
+++ api/__tests__/routes/todo.spec.ts
@@ -0,0 +1,43 @@
+import request from "supertest"
+
+import app from "../../src/app"
+import { apiStore } from "../../src/db"
+import { bcrypt, generateUUID } from "../../src/utils/tools"
+
+const credentials = { identifier: "abc-testlocalhostcom", password: "n9wb@DTJ.MLZ3" }
+
+describe("Todo controller", () => {
+  let authorization: string
+
+  beforeAll(async () => {
+    await apiStore.prepare("DELETE FROM Users WHERE identifier = ?").run(credentials.identifier)
+    const hash = await bcrypt.hash(credentials.password)
+    await apiStore.prepare("INSERT INTO Users (userId, identifier, password) VALUES (?,?,?)").run(generateUUID(""), credentials.identifier, hash)
+  })
+
+  test("connect to app and await provided token", async () => {
+    const res = await request(app).post("/api/auth/sign-in").send(credentials).set("Accept", "application/json").expect("Content-Type", /json/).expect(201)
+
+    authorization = res.header.authorization
+
+    expect(res.body).toEqual({
+      success: true,
+      __toastify: [{ type: "success", message: "Connecté" }],
+    })
+  })
+
+  test("check authorisation token", async () => {
+    console.log("provided token: ", authorization)
+
+    expect(authorization.split(" ")[0]).toEqual("Bearer")
+    expect(authorization.slice(7, authorization.length).split(".").length).toEqual(3)
+  })
+
+  describe("[GET] /api/todo/:todoId", () => {
+    test("returns the list matching my auth token", async () => {
+      expect("").toEqual("")
+    })
+  })
+
+  afterAll(() => apiStore.prepare("DELETE FROM Users WHERE identifier = ?").run(credentials.identifier))
+})
--- api/src/db.ts
+++ api/src/db.ts
@@ -8,7 +8,27 @@ apiStore.pragma("journal_mode = WAL") // performance
 export function createDatabase() {
   const createTables: string[] = [
     "Users (userId TEXT type PRIMARY KEY, identifier TEXT type UNIQUE, password TEXT, createdAt DATETIME DEFAULT CURRENT_TIMESTAMP)",
+    "Todos (todoId TEXT type PRIMARY KEY, label TEXT, status BOOLEAN, createdAt DATETIME DEFAULT CURRENT_TIMESTAMP, userId TEXT, FOREIGN KEY(userId) REFERENCES Users(userId))",
   ]
 
   createTables.forEach((table) => apiStore.exec(`CREATE TABLE IF NOT EXISTS ${table}`))
+
+  // apiStore.prepare("INSERT INTO Todos (label, status) VALUES (?,?)").run("abc", 0)
 }
+
+// FOREIGN KEY(ownerId) REFERENCES album(userId)
+
+// CREATE TABLE album(
+//   albumartist TEXT,
+//   albumname TEXT,
+//   albumcover BINARY,
+//   PRIMARY KEY(albumartist, albumname)
+// );
+
+// CREATE TABLE song(
+//   songid     INTEGER,
+//   songartist TEXT,
+//   songalbum TEXT,
+//   songname   TEXT,
+//   FOREIGN KEY(songartist, songalbum) REFERENCES album(albumartist, albumname)
+// );
