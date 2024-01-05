2ecf1e7e7cfabeab25f1ac1cf6695dd7d06b569b
julien.j <julien@vtest.com>
2024-01-06 00:48:02 +0100

    fix remove annotations

--- api/src/db.ts
+++ api/src/db.ts
@@ -12,23 +12,4 @@ export function createDatabase() {
   ]
 
   createTables.forEach((table) => apiStore.exec(`CREATE TABLE IF NOT EXISTS ${table}`))
-
-  // apiStore.prepare("INSERT INTO Todos (label, status) VALUES (?,?)").run("abc", 0)
 }
-
-// FOREIGN KEY(ownerId) REFERENCES album(userId)
-
-// CREATE TABLE album(
-//   albumartist TEXT,
-//   albumname TEXT,
-//   albumcover BINARY,
-//   PRIMARY KEY(albumartist, albumname)
-// );
-
-// CREATE TABLE song(
-//   songid     INTEGER,
-//   songartist TEXT,
-//   songalbum TEXT,
-//   songname   TEXT,
-//   FOREIGN KEY(songartist, songalbum) REFERENCES album(albumartist, albumname)
-// );
