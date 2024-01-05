a9fb0037ac5835472ca0c756295e51d061c55e19
julien.j <julien@vtest.com>
2024-01-05 17:51:49 +0100

    chore env files

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -2,7 +2,7 @@ import { storeToRefs } from "pinia"
 import { createRouter, createWebHistory } from "vue-router"
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.VITE_BASE_URL as string),
   routes,
 })
 
--- front/src/services/api.ts
+++ front/src/services/api.ts
@@ -3,7 +3,7 @@ import type { AxiosError, AxiosInstance, InternalAxiosRequestConfig } from "axio
 import type { CustomAxiosResponse } from "@/types/axios"
 
 const api = Axios.create({
-  baseURL: "http://127.0.0.1:8000/",
+  baseURL: import.meta.env.VITE_API_URL as string,
   headers: {
     "content-type": "application/json",
   },
