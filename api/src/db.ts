704e52ad65747a5ead497994a4c757861b06ed22
julien.j <julien@vtest.com>
2024-01-05 11:45:36 +0100

    feat: add ondelete cascade to db

--- api/src/db.ts
+++ api/src/db.ts
@@ -8,7 +8,7 @@ apiStore.pragma("journal_mode = WAL") // performance
 export function createDatabase() {
   const createTables: string[] = [
     "Users (userId TEXT type PRIMARY KEY, identifier TEXT type UNIQUE, password TEXT, createdAt DATETIME DEFAULT CURRENT_TIMESTAMP)",
-    "Todos (todoId TEXT type PRIMARY KEY, label TEXT, status BOOLEAN, createdAt DATETIME DEFAULT CURRENT_TIMESTAMP, userId TEXT, FOREIGN KEY(userId) REFERENCES Users(userId))",
+    "Todos (todoId TEXT type PRIMARY KEY, label TEXT, status BOOLEAN, createdAt DATETIME DEFAULT CURRENT_TIMESTAMP, userId TEXT, FOREIGN KEY(userId) REFERENCES Users(userId) ON DELETE CASCADE)",
   ]
 
   createTables.forEach((table) => apiStore.exec(`CREATE TABLE IF NOT EXISTS ${table}`))
