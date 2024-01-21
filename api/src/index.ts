1ba207e1af667f9aa6b563763322ccceeee09961
julien.j <julien@vtest.com>
2024-01-21 18:28:22 +0100

    implement ssl

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,3 +1,21 @@
+import { createServer as createHTTP } from "http"
+import { createServer as createHTTPS } from "https"
+import fs from "fs"
+import dotenv from "dotenv"
+dotenv.config()
+
 import app from "./app"
 
-app.listen(8000, () => console.log("API listening on HTTP PORT 8000"))
+const httpsOptions = {
+  key: fs.readFileSync("./ssl/key.pem"),
+  cert: fs.readFileSync("./ssl/cert.pem"),
+}
+const httpPort = Number(process.env.HTTP_PORT)
+const httpsPort = Number(process.env.HTTPS_PORT)
+
+if (!isNaN(httpPort)) createHTTP(app).listen(httpPort, () => console.warn(`API listening on HTTP PORT ${httpPort}`))
+try {
+  if (!isNaN(httpsPort)) createHTTPS(httpsOptions, app).listen(httpsPort, () => console.warn(`API listening on HTTPS PORT ${httpsPort}`))
+} catch (error) {
+  console.log(error)
+}
